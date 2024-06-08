96dd37b8dae6ab35e5c4b8aa2bb25bc318af7b34
ppppppsmash <pppppp.work@gmail.com>
2024-06-08 22:26:00 +0900

    feat: loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
+import { getAllPokemon } from './utils/pokemon';
+
 import './App.css';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      //all pokemon get
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+      setLoading(false);
+    }
+    fetchPokemonData();
+  }, []);
 
   return (
     <div className="App">
-      
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <h1>Got the data</h1>
+      )}
     </div>
   );
 }
